9112b7f49e810114a208d66e0e1081277912e661
JoelEnrique15 <joel_enrique1504@hotmail.com>
2016-09-28 22:46:57 -0500

    laboratorio 3
    dvñn

---
+++ laboratorios/laboratorio3.py
@@ -0,0 +1,15 @@
+a = input("  ")
+
+if a == a.upper():
+    if len(a) >=2:
+        print("Juan: Chillea")
+
+if not (a):
+        print("Mmm")
+
+if a.endswith("?"):
+        print("Ofi")
+
+elif a == a.lower():
+   if len(a) >=1:
+        print(" Juan: me da igual")
